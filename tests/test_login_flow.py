d1b48c50b60d1b9d8eeb1aa245bcee8cf22b04a2
vaibhav.jhingran <vaibhav.jhingran@go-jek.com>
2019-09-16 12:39:02 +0530

    Added new test for user login

---
+++ tests/test_login_flow.py
@@ -0,0 +1,20 @@
+import unittest
+from tests.test_setup import TestSetup
+from pages.loginpage import LoginPage
+from test_data.user_details import UserDetails
+
+
+class LoginTests(TestSetup):
+
+    def test_login_user(self):
+        login = LoginPage(self.driver)
+        login.click_login()
+        login.enter_username(UserDetails.USERNAME)
+        login.enter_password(UserDetails.PASSWORD)
+        login.login_user()
+
+
+if __name__ == '__main__':
+    TestSetup.setUp()
+    TestSetup.tearDown()
+    unittest.main(verbosity=2)
