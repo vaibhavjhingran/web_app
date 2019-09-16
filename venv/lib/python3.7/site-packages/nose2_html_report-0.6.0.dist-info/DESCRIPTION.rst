d8be89f500fc9fcdac06f9fe155f7768923c2c93
vaibhav.jhingran <vaibhav.jhingran@go-jek.com>
2019-09-16 08:37:43 +0530

    Created project repo
    * Added a simple .gitignore file
    * Set up virtual env folder and set up some python packages. Frozen them in requirements.txt

---
+++ venv/lib/python3.7/site-packages/nose2_html_report-0.6.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,54 @@
+# Nose2 HTML Report
+[![Build Status](https://travis-ci.org/mgrijalva/nose2-html-report.svg?branch=master)](https://travis-ci.org/mgrijalva/nose2-html-report)
+[![Coverage Status](https://coveralls.io/repos/github/mgrijalva/nose2-html-report/badge.svg?branch=master)](https://coveralls.io/github/mgrijalva/nose2-html-report?branch=master)
+
+### Introduction
+A [nose2](https://github.com/nose-devs/nose2) plugin for generating searchable HTML reports of your test results.
+- docstrings from tests are captured as test descriptions
+- if a test fails, the traceback will be captured in the report
+- report is searchable and filterable by passed/failed/error/etc.
+
+![Report Screenshot](https://raw.githubusercontent.com/mgrijalva/nose2-html-report/master/docs/images/report.png)
+
+### Installation
+You can install the Nose2 HTML Report Plugin using `pip`:
+```
+pip install nose2-html-report
+```
+
+### Configuration
+To get `nose2` to recognize the plugin add an entry into the `plugin` key of the `unittest` section of your `nose2.cfg` file. Configurations for the plugin should be placed into an `html-report` section of the configuration file. Below is a working example:
+```
+[unittest]
+plugins = nose2_html_report.html_report
+
+[html-report]
+always-on = True
+```
+
+#### Additional Settings
+Specify the path for the HTML report. Defaults to `report.html`
+```
+[unittest]
+plugins = nose2_html_report.html_report
+
+[html-report]
+always-on = True
+path = test_results/my_custom_report_file.html
+```
+
+### Usage
+Command line flag:
+```
+nose2 --html-report
+```
+
+If you have `always-on=True` inside your `nose2.cfg`:
+```
+nose2
+```
+
+### Contributing
+This is a small side project of mine. Feel free to submit any pull requests.
+
+
---
+++ venv/lib/python3.7/site-packages/nose2_html_report/html_report.py
@@ -0,0 +1,115 @@
+import json
+import logging
+import copy
+import os
+import traceback
+import unittest
+from nose2.events import Plugin
+from datetime import datetime
+
+from .render import load_template, render_template
+
+logger = logging.getLogger(__name__)
+
+
+class HTMLReporter(Plugin):
+    configSection = 'html-report'
+    commandLineSwitch = (None, 'html-report', 'Generate an HTML report containing test results')
+
+    def __init__(self, *args, **kwargs):
+        super(HTMLReporter, self).__init__(*args, **kwargs)
+        self.summary_stats = {'total': 0}
+        self.test_results = []
+        default_template_path = os.path.join(os.path.dirname(__file__), 'templates', 'report.html') 
+
+        self._config = {
+            'report_path': os.path.realpath(self.config.as_str('path', default='report.html')),
+            'template':  os.path.realpath(self.config.as_str('template', default=default_template_path))
+        }
+
+    def _sort_test_results(self):
+        return sorted(self.test_results, key=lambda x: x['name'])
+
+    def _generate_search_terms(self):
+        """
+        Map search terms to what test case(s) they're related to
+
+        Returns:
+            dict: maps search terms to what test case(s) it's relevant to
+
+        Example:
+        {
+            '12034': ['ui.tests.TestSomething.test_hello_world'],
+            'buggy': ['ui.tests.TestSomething.test_hello_world', 'ui.tests.TestSomething.buggy_test_case'],
+            'ui.tests.TestAnother.test_fail': ['ui.tests.TestAnother.test_fail']
+        }
+        """
+        search_terms = {}
+
+        for test_result in self.test_results:
+            # search for the test name itself maps to the test case
+            search_terms[test_result['name']] = test_result['name']
+
+            if test_result['description']:
+                for token in test_result['description'].split():
+                    if token in search_terms:
+                        search_terms[token].append(test_result['name'])
+                    else:
+                        search_terms[token] = [test_result['name']]
+
+        return search_terms
+
+    def testOutcome(self, event):
+        """
+        Reports the outcome of each test
+        """
+        test_case_import_path = event.test.id()
+
+        # Ignore _ErrorHolder (for arbitrary errors like module import errors),
+        # as there will be no doc string in these scenarios
+        test_case_doc = None
+        if not isinstance(event.test, unittest.suite._ErrorHolder):
+            test_case_doc = event.test._testMethodDoc
+
+        formatted_traceback = None
+        if event.outcome in ['failed', 'error']:
+            if event.exc_info:
+                exception_type = event.exc_info[0]
+                exception_message = event.exc_info[1]
+                exception_traceback = event.exc_info[2]
+                formatted_traceback = ''.join(traceback.format_exception(
+                    exception_type, exception_message, exception_traceback))
+
+        if event.outcome in self.summary_stats:
+            self.summary_stats[event.outcome] += 1
+        else:
+            self.summary_stats[event.outcome] = 1
+        self.summary_stats['total'] += 1
+
+        self.test_results.append({
+            'name': test_case_import_path,
+            'description': test_case_doc,
+            'result': event.outcome,
+            'traceback': formatted_traceback,
+            'metadata': copy.copy(event.metadata)
+        })
+
+    def afterSummaryReport(self, event):
+        """
+        After everything is done, generate the report
+        """
+        logger.info('Generating HTML report...')
+
+        sorted_test_results = self._sort_test_results()
+
+        context = {
+            'test_report_title': 'Test Report',
+            'test_summary': self.summary_stats,
+            'test_results': sorted_test_results,
+            'autocomplete_terms': json.dumps(self._generate_search_terms()),
+            'timestamp': datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC')
+        }
+        template = load_template(self._config['template'])
+        rendered_template = render_template(template, context)
+        with open(self._config['report_path'], 'w') as template_file:
+            template_file.write(rendered_template)
